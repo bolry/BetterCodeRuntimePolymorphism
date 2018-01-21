cea10932595d553ec59b372c8f19323263bf4ef8
Bo Rydberg <bolry@hotmail.com>
2018-01-21 23:33:43 +0100

    Add my_class_t only to client code

--- BetterCodeRuntimePolymorphism.cpp
+++ BetterCodeRuntimePolymorphism.cpp
@@ -11,6 +11,11 @@
 using namespace SaabAB;
 using namespace ewcstl;
 
+class my_class_t {
+};
+
+void draw(my_class_t const&, std::ostream& out, std::size_t position);
+
 int main()
 {
 	document_t document;
@@ -18,9 +23,16 @@ int main()
 	document.emplace_back(0);
 	document.emplace_back(std::string("Hello"));
 	document.emplace_back(2);
-	document.emplace_back(3);
+	document.emplace_back(my_class_t());
 
 	draw(document, std::cout, 0);
+}
+
+// ----------------------------------------------------------------------------
+
+void draw(my_class_t const&, std::ostream& out, std::size_t position)
+{
+	out << std::string(position, ' ') << "my_class_t\n";
 }
 
 // ----------------------------- END-OF-FILE ----------------------------------
